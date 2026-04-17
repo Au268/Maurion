aa629f79ed2cca803e985c0626ea85a1a22a7991
Ali Ubaid <ubaid.sgd321@gmail.com>
2026-04-17 17:25:45 +0500

    pushed 28

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
           <Route path="/category/:category" element={<Category />} />
           <Route path="/product" element={<Product />} />
           <Route path="/product/:id" element={<Product />} />
